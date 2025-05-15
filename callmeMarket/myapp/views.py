f2be94d31e110420b262a44518472918ef71dbf5
AnthonyGoc <anthonygabrieloliveiracruz@gmail.com>
2025-05-14 22:51:32 -0400

    Criando register/login/profile

--- callmeMarket/myapp/views.py
+++ callmeMarket/myapp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm
 
 # Create your views here.
 from django.http import HttpResponse
@@ -11,3 +14,32 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'myapp/index.html')
 
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'myapp/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('profile')
+        else:
+            return render(request, 'myapp/login.html', {'error': 'Usuário ou senha inválidos'})
+    return render(request, 'myapp/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def profile_view(request):
+    return render(request, 'myapp/profile.html', {'user': request.user})
